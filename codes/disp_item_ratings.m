6eff757326feac323dccbc84bfd692d526971723
Thanh Doan <tcdoan@gmail.com>
2011-04-17 05:50:35 -0500

    code to display user and item stats

---
+++ codes/disp_item_ratings.m
@@ -0,0 +1,36 @@
+% plot histogram of item ratings
+% load transpose.mat and mu.mat first
+while (true) 
+    item = input('Enter item id: ');    
+    if item == -1
+        break;
+    end
+
+    item_ratings = nonzeros(T(:,item)); 
+    num_disp = min(100,size(item_ratings,1));
+    fprintf('List of first %d item ratings: ', num_disp );
+    fprintf('%3.0f,',item_ratings(1:num_disp));
+    fprintf('\n');
+    
+    edges = 0:10:110;
+    labels = strcat(num2str((0:10:100)','%d'),{'s'});
+    fprintf('Tabulation: \n');    
+    tabulate(ordinal(item_ratings,labels,[],edges));
+   
+    num_ratings = size(item_ratings,1);
+    mean_rating = mean(item_ratings);
+    item_bias = mean_rating - MU;
+    sd_rating = std(item_ratings);
+    
+    min_rating = min(item_ratings);
+    max_rating = max(item_ratings);
+    median_rating = median(item_ratings);
+    mode_rating = mode(item_ratings);
+    
+    item_arr =  [num_ratings, mean_rating, item_bias, sd_rating, min_rating, max_rating, median_rating, mode_rating];
+    item_stat = dataset({item_arr, 'num_ratings', 'mean', 'item_bias', 'std', 'min', 'max', 'median', 'mode'});
+    fprintf('Summary statistics: \n');
+    disp(item_stat);
+    
+    hist(item_ratings);
+end
---
+++ codes/disp_user_ratings.m
@@ -0,0 +1,34 @@
+% plot histogram of user ratings
+% load trainAll.mat and mu.mat first
+while (true) 
+    user_id = input('Enter user id: ');    
+    if user_id == -1
+        break;
+    end
+
+    user_ratings = nonzeros(K(:,user_id)); 
+    fprintf('List of user ratings: \n');
+    disp(K(:,user_id));
+    
+    edges = 0:10:110;
+    labels = strcat(num2str((0:10:100)','%d'),{'s'});
+    fprintf('Tabulation: \n');    
+    tabulate(ordinal(user_ratings,labels,[],edges));
+   
+    num_ratings = size(user_ratings,1);
+    mean_rating = mean(user_ratings);
+    user_bias = mean_rating - MU;
+    sd_rating = std(user_ratings);
+    
+    min_rating = min(user_ratings);
+    max_rating = max(user_ratings);
+    median_rating = median(user_ratings);
+    mode_rating = mode(user_ratings);
+    
+    user_arr =  [num_ratings, mean_rating, user_bias, sd_rating, min_rating, max_rating, median_rating, mode_rating];
+    user_stat = dataset({user_arr, 'num_ratings', 'mean', 'user_bias', 'std', 'min', 'max', 'median', 'mode'});
+    fprintf('Summary statistics: \n');
+    disp(user_stat);
+    
+    hist(user_ratings);
+end
