95542967fba54ea9dbda230c10f1c23bdf5ce058
Thanh Doan <tcdoan@gmail.com>
2011-04-17 10:03:58 -0500

    update code to compute user and item stats, adding std deviation

--- codes/compute_item_stats.m
+++ codes/compute_item_stats.m
@@ -8,23 +8,27 @@ tic
 % row_id is the item number
 % column 1: # of ratings
 % column 2: mean score. average rating.
-% column 3: min score 
-% column 4: max score 
-% column 5: median score 
-% column 6: mode score 
-% column 7: item bias
+% column 3: item bias
+% column 4: std deviation 
+% column 5: min score 
+% column 6: max score 
+% column 7: median score 
+% column 8: mode score 
 
-items = zeros(nItems, 7, 'single');
+items = zeros(nItems, 8, 'single');
 for j=1:nItems
         if nnz(T(:,j)) > 0
             item = nonzeros(T(:,j));
-            items(j, 1) = sum(item > 0);
+            
+            items(j, 1) = nnz(T(:,j));
             items(j, 2) = mean(item);
-            items(j, 3) = min(item);
-            items(j, 4) = max(item);
-            items(j, 5) = median(item);
-            items(j, 6) = mode(item);
-            items(j, 7) = mean(item)-MU;
+            items(j, 3) = items(j, 2)-MU;
+            items(j, 4) = std(item);
+            
+            items(j, 5) = min(item);
+            items(j, 6) = max(item);
+            items(j, 7) = median(item);
+            items(j, 8) = mode(item);
         end
 end
 
--- codes/compute_user_stats.m
+++ codes/compute_user_stats.m
@@ -1,29 +1,32 @@
 nUsers=1000990;  
-
 outfile = 'users.mat';
-
 tic
 
 % create a dense matrix users from sparse matrix K
 % row_id is the user number
-% column 1: # of ratings
-% column 2: mean score. average rating.
-% column 3: min score 
-% column 4: max score 
-% column 5: median score 
-% column 6: mode score 
-% column 7: user bias
-users = zeros(nUsers, 7, 'single');
+% 1. num_ratings = size(user_ratings,1);
+% 2. mean_rating = mean(user_ratings);
+% 3. user_bias = mean_rating - MU;
+% 4. std_rating = std(user_ratings); 
+% 5. min_rating = min(user_ratings);
+% 6. max_rating = max(user_ratings);
+% 7. median_rating = median(user_ratings);
+% 8. mode_rating = mode(user_ratings);
+    
+users = zeros(nUsers, 8, 'single');
 for j=1:nUsers
     user = nonzeros(K(:,j)); 
     %hist(user)    
-    users(j, 1) = sum(user > 0);
+    users(j, 1) = nnz(K(:,j));
     users(j, 2) = mean(user);
-    users(j, 3) = min(user);
-    users(j, 4) = max(user);
-    users(j, 5) = median(user);
-    users(j, 6) = mode(user);
-    users(j, 7) = mean(user)-MU;
+    users(j, 3) = users(j, 2)-MU; % user bias
+    users(j, 4) = std(user);
+ 
+    users(j, 5) = min(user);
+    users(j, 6) = max(user);
+    users(j, 7) = median(user);
+    users(j, 8) = mode(user);
+    
 end
 
 save(outfile,'-v7.3','users');
