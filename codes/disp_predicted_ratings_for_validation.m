b576ee32c3fe3b4ac6fdfde38b58c112da07ccec
Thanh Doan <tcdoan@gmail.com>
2011-04-18 18:24:00 -0500

    combine predicted raings and observed ratings into a dataset to before displaying it

--- codes/disp_predicted_ratings_for_validation.m
+++ codes/disp_predicted_ratings_for_validation.m
@@ -18,35 +18,34 @@ while (true)
         break;
     end
     
-    fprintf('true rating for user %d: \n', user_id);
-    disp(V(:,user_id)); % display observed rating in the validation set
+   
+    [ids, uids, tRatings] = find(V(:,user_id));
+    [~, ~, pRatings] = find(P(:,user_id));    
+    all_ratings = [ids, uids, pRatings, tRatings, pRatings-tRatings];
+    rating_report = dataset({all_ratings, 'item_id', 'user_id', 'predicted_rating', 'true_rating', 'predicted_error'});    
     
-    [ids, ~, ~] = find(V(:,user_id));
     fprintf('training/estimation statistics for user %d: \n', user_id);
-    %disp(users(user_id,:));
     user_arr =  [user_id, users(user_id,:)];
     user_stat = dataset({user_arr, 'user_id', 'num_ratings', 'mean', 'user_bias', 'std', 'min', 'max', 'median', 'mode'});
     disp(user_stat);    
         
     fprintf('validation statistics for user %d: \n', user_id);
-    %disp(vusers(user_id,:));
     vuser_arr =  [user_id, vusers(user_id,:)];
     vuser_stat = dataset({vuser_arr, 'user_id', 'num_ratings', 'mean', 'user_bias', 'std', 'min', 'max', 'median', 'mode'});
     disp(vuser_stat);
         
     fprintf('training statistics for associated items \n');
-    %disp(items(ids,:));
     item_arr = [ids, items(ids,:)];
     item_stat = dataset({item_arr, 'item_id', 'num_ratings', 'mean', 'item_bias', 'std', 'min', 'max', 'median', 'mode'});    
     disp(item_stat);
     
     fprintf('validation statistics for associated items \n');
-    %disp(vitems(ids,:));        
     vitem_arr = [ids, vitems(ids,1:8)];
     vitem_stat = dataset({vitem_arr, 'item_id', 'num_ratings', 'mean', 'item_bias', 'std', 'min', 'max', 'median', 'mode'});    
     disp(vitem_stat);
     
-    fprintf('predicted ratings for user %d \n', user_id);
-    disp(P(:,user_id)); % display predicted ratings     
+    fprintf('predicted ratings vs true ratings for user %d \n', user_id); 
+    disp(rating_report);
+    
 end
 
